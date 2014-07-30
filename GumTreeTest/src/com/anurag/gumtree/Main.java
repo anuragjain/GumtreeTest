3b04b02dcedb7b56b797ac64e38328beb6041328
anurjain <anurjain@paypal.com>
2014-07-31 00:09:09 +0530

    version 1

--- GumTreeTest/src/com/anurag/gumtree/Main.java
+++ GumTreeTest/src/com/anurag/gumtree/Main.java
@@ -1,24 +1,25 @@
 package com.anurag.gumtree;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
 
 	List<Person> persons = new ArrayList<Person>();
 
-	public static void main(int arg, String[] args){
+	public static void main(String[] args) {
 		Main m = new Main();
 		m.readData();
+		System.out.println("Number of Males: "+m.getNumOfMales());
+		System.out.println("Oldest Person: "+m.getOldestPerson());
+		System.out.println("Bill is older than Paul by "+m.getDaysDiffPaulBill()+" days");
 	}
 
 	public int getNumOfMales(){
@@ -29,17 +30,44 @@ public class Main {
 		}
 		return count;
 	}
-		
+
+	public String getOldestPerson(){
+		String out = "";
+		long date = new Date().getTime();
+		for(Person p:persons){
+			if(p.getDateOfBirth()<date){
+				date = p.getDateOfBirth();
+				out = p.getFirst_name()+" "+p.getLast_name();
+			}
+		}
+		return out;
+	}
+
+	public int getDaysDiffPaulBill(){
+		long dob_paul = 0;
+		long dob_bill = 0;
+		for(Person p:persons){
+			if(p.getFirst_name().equalsIgnoreCase("paul"))
+				dob_paul = p.getDateOfBirth();
+			if(p.getFirst_name().equalsIgnoreCase("bill"))
+				dob_bill = p.getDateOfBirth();
+		}
+		int diff = (int) ((dob_paul-dob_bill)/(1000*3600*24));
+		return diff;
+	}
+
 	public void readData(){
 		try {
-			FileInputStream fis = new FileInputStream(new File("/AddressBook"));
+			URL url = getClass().getResource("AddressBook");
+			FileInputStream fis = new FileInputStream(new File(url.getPath()));
 			BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
 			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
 			String line;
 			while ((line = reader.readLine()) != null){
 				String[] temp = line.split(",");
+				String[] name = temp[0].split(" ");
 				String dob = temp[2].trim();
-				Person p = new Person(temp[0].trim(), temp[1].trim().toLowerCase(), formatter.parse(dob).getTime());
+				Person p = new Person(name[0].trim(),name[1].trim(), temp[1].trim().toLowerCase(), formatter.parse(dob).getTime());
 				persons.add(p);
 			}
 			reader.close();
--- GumTreeTest/src/com/anurag/gumtree/Person.java
+++ GumTreeTest/src/com/anurag/gumtree/Person.java
@@ -2,24 +2,37 @@ package com.anurag.gumtree;
 
 
 public class Person {
-	String name;
+	String first_name;
+	String last_name;
 	String sex;
 	long dateOfBirth;
 
 
-	public Person(String name, String sex, long dateOfBirth) {
+	public Person(String first_name, String last_name, String sex, long dateOfBirth) {
 		super();
-		this.name = name;
+		this.first_name = first_name;
+		this.last_name = last_name;
 		this.sex = sex;
 		this.dateOfBirth = dateOfBirth;
 	}
 
-	public String getName() {
-		return name;
+
+	public String getFirst_name() {
+		return first_name;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
 	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
 	public String getSex() {
 		return sex;
 	}
